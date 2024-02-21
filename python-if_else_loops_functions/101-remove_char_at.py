c319aff3aa9eb485b7a71297b57845c34e43c023
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2024-02-21 05:43:40 -0800

    whitespacomg

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3                                                                                                                         
-def remove_char_at(str, n):
-    if n>=0:
-        x = str[:n] + str[n + 1:]
+#!/usr/bin/python3
+def remove_char_at(string, n):
+    if n >= 0:
+        x = string[:n] + string[n + 1:]
     else:
-        x = str
+        x = string
     return x

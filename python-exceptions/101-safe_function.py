c5c1f7bcff8add3049a77011463a12f0dde9e16b
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2024-02-21 10:19:42 -0800

    exceptions

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        output = fct(*args)
+    except Exception as E:
+        print("Exception: {}".format(E), file=sys.stderr)
+        return None
+    else:
+        return output
